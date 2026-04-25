111453dd0ef4d7d634ef30b08fe7e67f1afc7e98
MartinD-git <160508170+MartinD-git@users.noreply.github.com>
2026-04-25 19:59:23 +0000

    Deploying to gh-pages from @ MartinD-git/MartinD-git.github.io@c0a394f3c56880695b4e2bd03a9e4ad5ebb43655 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://your-username.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://martind-git.github.io//#contact">contact us</a>.'
               }
             ]
           }
